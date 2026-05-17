11992150ff9a9a40c87d90e9a6c393bef4cf8a84
Casper <casper@gmail.com>
2026-05-17 16:33:54 +0300

    Fix ESLint configuration

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -6,6 +6,15 @@ module.exports = [
             ecmaVersion: 2021,
             sourceType: "commonjs"
         },
+        languageOptions: {
+             ecmaVersion: 2021,
+             sourceType: "commonjs",
+             globals: {
+             console: "readonly",
+             require: "readonly",
+            module: "readonly"
+      }
+},
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "error",
