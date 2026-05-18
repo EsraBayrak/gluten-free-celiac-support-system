c0f6d3ab2a305a321267727522933e09d3e66106
Casper <casper@gmail.com>
2026-05-18 17:38:15 +0300

    Fix Jest ESLint globals

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -10,10 +10,13 @@ module.exports = [
              ecmaVersion: 2021,
              sourceType: "commonjs",
              globals: {
-             console: "readonly",
-             require: "readonly",
-            module: "readonly"
-      }
+    console: "readonly",
+    require: "readonly",
+    module: "readonly",
+    describe: "readonly",
+    test: "readonly",
+    expect: "readonly"
+}
 },
         rules: {
             "no-unused-vars": "warn",
